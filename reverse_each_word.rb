88fc9ea64f099418c5c8283ef12f20068c9096a2
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-30 20:06:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,14 @@ def reverse_each_word(sentence)
   reversed_sentence = reversed_array.join(" ")
   return reversed_sentence
 end
+
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(" ")
+  reversed_array = []
+  sentence_array.collect do |word|
+    reversed_word = word.reverse
+    reversed_array << reversed_word
+    reversed_sentence = reversed_array.join(" ")
+    return reversed_sentence
+  end
+end
